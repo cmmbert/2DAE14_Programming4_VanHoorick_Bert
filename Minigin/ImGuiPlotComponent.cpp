9a17123ae23a18e7f6abc3e8c6217b553ff75d15
cmmbert <cmmbert@hotmail.be>
2022-03-01 18:28:20 +0100

    removed commented code

--- Minigin/ImGuiPlotComponent.cpp
+++ Minigin/ImGuiPlotComponent.cpp
@@ -19,31 +19,7 @@ void dae::ImGuiPlotComponent::Update()
 
 void dae::ImGuiPlotComponent::Render() const
 {
-	//ImGui::PlotConfig conf;
-	//std::vector<float> y_data{};
-	//for (auto it = m_PlotValues.begin(); it != m_PlotValues.end(); ++it)
-	//{
-	//	y_data.insert(y_data.end(), it->second.begin(), it->second.end());
-	//}
-
-
-	//conf.values.ys_list = y_data.data();
-
-	//conf.values.count = static_cast<int>( it->second.size()) + 1;
-	//conf.scale.min = 0;
-	//float max = *std::ranges::max_element(it->second);
-	//conf.scale.max = max * 1.1f;
-	//conf.tooltip.show = true;
-	//conf.tooltip.format = "x=%.2f, y=%.2f";
-	//conf.grid_x.show = true;
-	//conf.grid_y.show = true;
-	//conf.frame_size = ImVec2(400, 200);
-	//conf.line_thickness = 2.f;
-
-	//ImGui::Plot(it->first.c_str(), conf);
-
-
-	//ONLY RENDERS ONE LINE TODO: use commented code above to support multiple lines
+	//ONLY RENDERS ONE LINE TODO: support multiple lines
 	ImGui::PlotConfig conf;
 	conf.values.ys = m_PlotValues.begin()->second.data();
 	conf.values.count = (int)m_PlotValues.begin()->second.size()+1;
