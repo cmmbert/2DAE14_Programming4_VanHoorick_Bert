c8f7da5b8af93bec9549b75c9e598a82a7724b80
cmmbert <cmmbert@hotmail.be>
2022-05-31 18:38:45 +0200

    sfdqsf

--- BurgerTime/BurgerShardComp.h
+++ BurgerTime/BurgerShardComp.h
@@ -6,7 +6,7 @@ class BurgerPiece;
 
 class BurgerShardComp : public dae::BaseComponent
 {
-	const int m_Nudge{ -m_pGameObject->GetTransform().GetSize().y/4 };
+	const int m_Nudge{ -m_pGameObject->GetTransform().GetSize().y/2/4 };
 	int m_Index;
 	int m_OriginalY;
 	int m_NudgeMultiplier{1};
